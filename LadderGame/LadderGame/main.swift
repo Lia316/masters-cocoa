58988fcd0e8124b894c522ddf7c02a0a568ab91d
Lia <judykim257@gmail.com>
2020-11-05 23:22:15 +0900

    인원 수 입력 및 에러 처리 함수 만들었습니다.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -2,10 +2,37 @@
 //  main.swift
 //  LadderGame
 //
-//  Created by 김지경 on 2020/11/05.
+//  Created by Lia on 2020/11/05.
 //
 
 import Foundation
 
-print("Hello, World!")
 
+enum LadderError : Error {
+    case invalidNum
+}
+
+// 인원수 입력 함수
+func inputHeadcount () throws -> Int{
+    
+    print("사다리 게임에 참여할 인원을 입력하세요 :")
+    var headcount = Int(readLine()!)!
+    print(headcount)
+    guard (headcount > 2 && headcount < 8) else {
+        throw LadderError.invalidNum
+    }
+    
+    return headcount
+}
+
+// error handling check
+do {
+    let s : Int = try inputHeadcount()
+    print(s)
+} catch LadderError.invalidNum {
+    print ("error")
+    exit(0)
+}
+
+// exit(0) check
+print (" 왜 안 되지")
