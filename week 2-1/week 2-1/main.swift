00e7eac4c56779113036ab9333fa0be92d7d705b
Lia <judykim257@gmail.com>
2020-11-09 22:30:19 +0900

    연습문제 2. 아이디 규칙 검사

--- week 2-1/week 2-1/main.swift
+++ week 2-1/week 2-1/main.swift
@@ -7,9 +7,37 @@
 
 import Foundation
 
-var myArray = ArrayPractice()
-myArray.printArray(array: myArray.fillArray())
+//var myArray = ArrayPractice()
+//myArray.printArray(array: myArray.fillArray())
+//
+//printArray(array: setBoolArray(), newChar: "$")
+//
+//printMagicSquare(arr: makeMagicSquare(squareNum: 3))
 
-printArray(array: setBoolArray(), newChar: "$")
+//let str = "abcDEF"
+//for index in str.utf16 {
+//    print(index)// 97 98 99 68 69
+//}
+//
+//var boolean = false
+//
+//boolean = str.contains("ab")
+//print(boolean)
+//
+//var arr = Array<UInt16>()
+//for index in str.utf16{
+//    arr.append(index)
+//}
+//print(arr)
+//
+//if(arr.contains(97)){
+//    print("can't")
+//}
 
-printMagicSquare(arr: makeMagicSquare(squareNum: 7))
+print(IDValidator(ID: "12sdre"))
+print(IDValidator(ID: "901dwe"))
+print(IDValidator(ID: "12"))
+print(IDValidator(ID: "dkdfjweoewnrsdlqwjioeqwhejqwirwioe"))
+print(IDValidator(ID: "1ERWEA-ewr13"))
+print(IDValidator(ID: "1g666dse"))
+print(IDValidator(ID: "1#dfsw"))
